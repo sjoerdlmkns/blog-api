581c93b5f0cc7222751177bb81171f785f25bf10
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-02 06:47:29 +0200

    feat: extracted more params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,34 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Ip,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
 
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
-  public createUsers(@Body() request: Request) {
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
     console.log(request);
+    console.log(headers);
+    console.log(ip);
+
     return 'You sent a post request to users endpoint';
   }
 }
