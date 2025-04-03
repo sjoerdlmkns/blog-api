4f7bde7285c22e216f88249348a42a28502a75dd
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-03 06:07:14 +0200

    feat: validation/transformation with pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,26 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
   Headers,
   Ip,
   Param,
+  ParseIntPipe,
   Post,
   Query,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
+  @Get('/:id?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log(limit);
+    console.log(page);
 
     return 'You sent a get request to users endpoint';
   }
