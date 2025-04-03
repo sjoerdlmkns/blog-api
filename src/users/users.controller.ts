d05cab2402bcb00ec065e0c071e61f465d70d61c
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-03 06:34:33 +0200

    feat: validate with createUserDto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,13 +3,13 @@ import {
   Controller,
   DefaultValuePipe,
   Get,
-  Headers,
-  Ip,
   Param,
   ParseIntPipe,
   Post,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -26,14 +26,8 @@ export class UsersController {
   }
 
   @Post()
-  public createUsers(
-    @Body() request: any,
-    @Headers() headers: any,
-    @Ip() ip: any,
-  ) {
-    console.log(request);
-    console.log(headers);
-    console.log(ip);
+  public createUsers(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+    console.log(createUserDto);
 
     return 'You sent a post request to users endpoint';
   }
