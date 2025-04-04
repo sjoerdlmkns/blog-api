5688ced5314cbeb3c25f22a86c03aeef88733865
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-04 06:13:09 +0200

    feat: param dtos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,17 +9,19 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUserParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(limit);
-    console.log(page);
+    console.log(getUserParamDto);
+    // console.log(limit);
+    // console.log(page);
 
     return 'You sent a get request to users endpoint';
   }
