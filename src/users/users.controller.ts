9beb77128a8ccc5e9dc7cf6e038e315c0b72f0b6
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-04 06:56:03 +0200

    feat: created and injected UserService

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,9 +12,15 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    // Injecting UsersService
+    private readonly usersService: UsersService,
+  ) {}
+
   @Get('/:id?')
   public getUsers(
     @Param() getUserParamDto: GetUsersParamDto,
