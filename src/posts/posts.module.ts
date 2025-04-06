80f61f6f91d2125fe5852a8657ae0368fae4fc61
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-06 08:20:53 +0200

    feat: inter-modular di posts/users

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { PostsController } from './posts.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   providers: [PostsService],
   controllers: [PostsController],
+  imports: [UsersModule],
 })
 export class PostsModule {}
--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
 export class PostsService {
+  constructor(
+    // Injecting Users Service
+    private readonly usersService: UsersService,
+  ) {}
   public findAll(userId: string) {
-    console.log(userId);
+    const user = this.usersService.findOneById(userId);
+
+    return [
+      {
+        title: 'Test title',
+        content: 'Test Content',
+        user: user,
+      },
+
+      {
+        title: 'Test title 2',
+        content: 'Test Content 2',
+        user: user,
+      },
+    ];
   }
 }
