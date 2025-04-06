738c0fb029f2f8d07cc7fc3b39f726993bf2c8e8
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-06 07:48:36 +0200

    feat: connect userService.fndAll to UsersController

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public findAll(
+    getUserParamDto: GetUsersParamDto,
+    limit: number,
+    page: number,
+  ) {
+    return [
+      {
+        firstName: 'John',
+        email: 'john@doe.com',
+      },
+      {
+        firstName: 'Alice',
+        email: 'alice@doe.com',
+      },
+    ];
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -27,11 +27,7 @@ export class UsersController {
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(getUserParamDto);
-    // console.log(limit);
-    // console.log(page);
-
-    return 'You sent a get request to users endpoint';
+    return this.usersService.findAll(getUserParamDto, limit, page);
   }
 
   @Post()
