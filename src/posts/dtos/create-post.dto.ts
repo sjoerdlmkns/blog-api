46f820c42952133470ca6864a1b371e9ec843a76
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-07 07:00:31 +0200

    feat: added class validator annotations to create post dto

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,15 +1,70 @@
 import { postType } from '../enums/postType.enum';
 import { postStatus } from '../enums/postStatus.enum';
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
+import { Type } from 'class-transformer';
 
 export class CreatePostDto {
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
   title: string;
+
+  @IsEnum(postType)
+  @IsNotEmpty()
   postType: postType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
+  })
   slug: string;
+
+  @IsEnum(postStatus)
+  @IsNotEmpty()
   status: postStatus;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsOptional()
+  @IsJSON()
   schema?: string;
+
+  @IsOptional()
+  @IsUrl()
   featuredImageUrl?: string;
-  publishOn: Date;
+
+  @IsISO8601()
+  @IsOptional()
+  publishOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({
+    each: true,
+  })
+  @MinLength(3, { each: true })
   tags?: string[];
-  metaOptions?: { key: string; value: any }[];
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreatePostMetaOptionsDto)
+  metaOptions?: CreatePostMetaOptionsDto[];
 }
