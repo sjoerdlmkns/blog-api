4994d750b0e0286276f219c8c58662ba3abcecc5
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-07 06:06:04 +0200

    feat: improved swagger docs by adding groups and setting config variables

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,17 @@ async function bootstrap() {
   );
 
   // Swagger configuration
-  const config = new DocumentBuilder().setVersion('1.0').build();
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Masterclass - Blog app API')
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense(
+      'MIT License',
+      'https://gist.githubusercontent.com/adamveld12/b64e74d14d7d9043b85c/raw/2d4cf3afe445b79c3b496ce67e9d2ab3365232dd/LICENSE.txt',
+    )
+    .addServer('http:localhost:3000')
+    .setVersion('1.0')
+    .build();
 
   // Instantiate Document
   const document = SwaggerModule.createDocument(app, config);
