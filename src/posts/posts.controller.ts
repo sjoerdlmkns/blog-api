fe3cf3ed63d746e75f6ca70daa78fa7849c8c59b
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-07 20:50:32 +0200

    feat: patch post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -18,11 +19,27 @@ export class PostsController {
 
   @Post()
   @ApiOperation({
-    summary: 'Creates a new post',
+    summary: 'Creates a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if your post is created successfully',
   })
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log('Create posts controller');
     console.log(createPostDto);
     return 'Created post';
+  }
+
+  @Patch()
+  @ApiOperation({
+    description: 'Updates an existing blog post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'A 200 response if the post is updated successfully.',
+  })
+  public updatePost(@Body() patchPostsDto: PatchPostDto) {
+    console.log(patchPostsDto);
   }
 }
