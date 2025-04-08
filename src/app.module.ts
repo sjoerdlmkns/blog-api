2515eec285d3360c4c67432c6c0136bd2d49477b
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-08 06:25:43 +0200

    feat: bootstrap typeorm to psql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      database: 'nestjs-blog',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
