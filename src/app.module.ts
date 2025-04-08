5a6cd36ef519f27eda169952a6e90adaa6daca00
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-08 06:53:23 +0200

    feat: created user entity, tweaked createUserDto

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [],
       useFactory: () => ({
         type: 'postgres',
-        entities: [],
+        entities: [User],
         synchronize: true,
         port: 5432,
         username: 'postgres',
