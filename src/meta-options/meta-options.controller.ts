56820840c3d3aa1b54620f1255841de9950f9666
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-10 06:25:32 +0200

    refactor: move MetaOptionsService into /providers

--- src/meta-options/meta-options.controller.ts
+++ src/meta-options/meta-options.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CreatePostMetaOptionsDto } from './dtos/create-post-meta-options.dto';
-import { MetaOptionsService } from './meta-options.service';
+import { MetaOptionsService } from './providers/meta-options.service';
 
 @Controller('meta-options')
 export class MetaOptionsController {
--- src/meta-options/meta-options.module.ts
+++ src/meta-options/meta-options.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MetaOptionsController } from './meta-options.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MetaOption } from './meta-option.entity';
-import { MetaOptionsService } from './meta-options.service';
+import { MetaOptionsService } from './providers/meta-options.service';
 
 @Module({
   controllers: [MetaOptionsController],
