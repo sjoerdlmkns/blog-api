c402a7a42689ee74eab6d31fd9199037e63ff436
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-10 06:20:50 +0200

    wip: createMetaOptions function on MetaOptionsController

--- src/meta-options/meta-options.controller.ts
+++ src/meta-options/meta-options.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreatePostMetaOptionsDto } from './dtos/create-post-meta-options.dto';
 
 @Controller('meta-options')
-export class MetaOptionsController {}
+export class MetaOptionsController {
+  @Post()
+  @ApiOperation({
+    summary: 'Creates a new meta option',
+  })
+  @ApiResponse({
+    status: 201,
+    description:
+      'You get a 201 response if your meta option is created successfully',
+  })
+  public createMetaOptions(
+    @Body() createMetaOptionsDto: CreatePostMetaOptionsDto,
+  ) {
+    console.log(createMetaOptionsDto);
+  }
+}
