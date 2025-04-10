11d5e3e8304eb3431499475af95cf5956f4bb277
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-10 06:15:37 +0200

    feat: relationship between Post and MetaOption entity

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,7 +1,13 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { postType } from './enums/postType.enum';
 import { postStatus } from './enums/postStatus.enum';
-import { CreatePostMetaOptionsDto } from '../meta-options/dtos/create-post-meta-options.dto';
+import { MetaOption } from 'src/meta-options/meta-option.entity';
 
 @Entity()
 export class Post {
@@ -64,7 +70,10 @@ export class Post {
   })
   publishOn?: Date;
 
+  @OneToOne(() => MetaOption)
+  @JoinColumn()
+  metaOptions?: MetaOption;
+
   // TODO: Fix relationships later.
   tags?: string[];
-  metaOptions?: CreatePostMetaOptionsDto[];
 }
