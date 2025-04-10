e49dc676e0e43954a408f92c88ad7ed0361a526f
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-10 06:04:13 +0200

    feat: autoLoadEntities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,7 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Post } from './posts/post.entity';
 import { TagsModule } from './tags/tags.module';
-import { Tag } from './tags/tag.entity';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 
 @Module({
@@ -22,7 +19,8 @@ import { MetaOptionsModule } from './meta-options/meta-options.module';
       inject: [],
       useFactory: () => ({
         type: 'postgres',
-        entities: [User, Post, Tag],
+        // entities: [User, Post, Tag],
+        autoLoadEntities: true,
         synchronize: true,
         port: 5432,
         username: 'postgres',
