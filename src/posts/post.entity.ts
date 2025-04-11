374a6f3c0992e1d171f4f278ebcd5e6a86843b82
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-11 06:33:54 +0200

    feat: OneToMany/ManyToOne relationship between User and Post

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,7 +1,14 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { postType } from './enums/postType.enum';
 import { postStatus } from './enums/postStatus.enum';
 import { MetaOption } from 'src/meta-options/meta-option.entity';
+import { User } from 'src/users/user.entity';
 
 @Entity()
 export class Post {
@@ -69,6 +76,9 @@ export class Post {
     eager: true,
   })
   metaOptions?: MetaOption;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  author: User;
 
   // TODO: Fix relationships later.
   tags?: string[];
