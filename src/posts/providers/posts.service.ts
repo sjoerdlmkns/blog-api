34be517a28f811763fbf12a8cfd6f6a65fbc0e64
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-11 05:56:07 +0200

    feat: get posts

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -27,21 +27,9 @@ export class PostsService {
     return await this.postsRepository.save(post);
   }
 
-  public findAll(userId: string) {
-    const user = this.usersService.findOneById(userId);
+  public async findAll(userId: string) {
+    const posts = await this.postsRepository.find();
 
-    return [
-      {
-        title: 'Test title',
-        content: 'Test Content',
-        user: user,
-      },
-
-      {
-        title: 'Test title 2',
-        content: 'Test Content 2',
-        user: user,
-      },
-    ];
+    return posts;
   }
 }
