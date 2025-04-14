06bb1edaa79e1a640f9c61242003f33c361e9a60
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-14 06:01:23 +0200

    feat: add create many users endpoint on UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -59,6 +59,11 @@ export class UsersController {
     return this.usersService.createUser(createUserDto);
   }
 
+  @Post()
+  public createManyUsers(@Body() createUsersDto: CreateUserDto[]) {
+    return this.usersService.createMany(createUsersDto);
+  }
+
   @Patch()
   public patchUser(@Body() patchUserDto: PatchUserDto) {
     return patchUserDto;
