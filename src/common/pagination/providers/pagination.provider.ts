696069b270d768187274818017f46ffba25eb640
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-15 06:21:31 +0200

    feat: PaginationProvider

--- src/common/pagination/providers/pagination.provider.ts
+++ src/common/pagination/providers/pagination.provider.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PaginationQueryDto } from '../dtos/pagination-query.dto';
+import { ObjectLiteral, Repository } from 'typeorm';
 
 @Injectable()
-export class PaginationProvider {}
+export class PaginationProvider {
+  public async paginateQuery<T extends ObjectLiteral>(
+    paginationQuery: PaginationQueryDto,
+    repository: Repository<T>,
+  ) {
+    const results = await repository.find({
+      skip: (paginationQuery.page - 1) * paginationQuery.limit,
+      take: paginationQuery.limit,
+    });
+
+    return results;
+  }
+}
