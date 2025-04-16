33df1030581e30cd4d245352543443c7036737ae
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-16 06:46:46 +0200

    chore: gen bycrypt provider and hashing provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { HashingProvider } from './providers/hashing.provider';
+import { BycryptProvider } from './providers/bycrypt.provider';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, HashingProvider, BycryptProvider],
   imports: [forwardRef(() => UsersModule)],
   exports: [AuthService],
 })
