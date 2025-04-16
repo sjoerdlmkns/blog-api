20fce3da2abce8f001c365e6b27964801f06ddb0
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-04-16 06:56:22 +0200

    feat: bcrypt hashing implementation

--- src/auth/providers/bycrypt.provider.ts
+++ src/auth/providers/bycrypt.provider.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { HashingProvider } from './hashing.provider';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class BycryptProvider {}
+export class BycryptProvider implements HashingProvider {
+  public async hashPassword(data: string | Buffer): Promise<string> {
+    // Generate a salt
+    const salt = await bcrypt.genSalt();
+    return bcrypt.hash(data, salt);
+  }
+  comparePassword(data: string | Buffer, encrypted: string): Promise<boolean> {
+    return bcrypt.compare(data, encrypted);
+  }
+}
